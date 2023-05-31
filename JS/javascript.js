bc1d8d46e789233c48d751d06a3786315403f8cf
dan.vsm <133366440+Blackvsm@users.noreply.github.com>
2023-05-31 16:22:26 -0300

    Fiz menu
    Eu fiz o Menu, falta algumas melhorias no click pensando na responsividade para cada aparelho

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,34 +1,60 @@
+
 function initialize() {   
-    dataHora()    
-    click()
-    setInterval( () => {
+    itensMenu()    
+    somClick()
+    clickMenu()
+
+    /*setInterval( () => {
              dataHora()
-        }, 0)
+        }, 0)*/
               
 }
 
-function dataHora () {
+function itensMenu() {
+    itens = document.querySelector('#sorte')
+    itens.addEventListener('mouseover', (e)=>{
+    console.log(e)
+        
+    if (menu.style.display === 'block') {
+            menu.style.display = 'none'
+        
+    } else {
+            menu.style.display = 'block'
+        }
+    })
+}
+
+
+/*function clickMenu() {
+
+
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none'
+    
+    } else {
+        menu.style.display = 'block'
+    }
+    somClick() 
+}*/
+ 
+/*function dataHora () {
     const display = document.getElementById('dataHora')
     const hora = new Date().toLocaleTimeString("pt-BR")
     
     display.innerHTML = 'Time: ' + hora
-}
+}*/
 
+function somClick () {
+    const clickSom = document.querySelector('#SomClick')
     
-    function click () {
-        const botton = document.querySelectorAll('.caixa')
-        const chute = document.getElementsByClassName('caixa')[1]
-        
-        
-        botton.forEach(btn=> {
-            btn.addEventListener('click', e => {
-                chute.play()
-                console.log(e)
-            })
-            console.log(btn)
-        })
+    const clickMenu = document.querySelector("#menu")
+    
+    clickMenu.addEventListener('onclick onkeypress', e=> {
+        console.log(e)
+    })
+    
 
-    }
+}
     
     
     
