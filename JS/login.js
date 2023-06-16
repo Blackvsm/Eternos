4a6f2886c70041997454c63bc2f846e1afdc49b5
dan.vsm <133366440+Blackvsm@users.noreply.github.com>
2023-06-16 14:33:17 -0300

    Login Funcional
    Agora consegui deixar o maximo possivel responsivo e fiz um login funcional

---
+++ JS/login.js
@@ -0,0 +1,39 @@
+const enviarBtn = document.querySelector('#enviarBtn');
+
+enviarBtn.addEventListener('click', obterDadosFormulario);
+
+function obterDadosFormulario() {
+   
+    const nomeInput = document.querySelector('#nome');
+    const senhaInput = document.querySelector('#senha');
+  
+    const nome = nomeInput.value;
+    const senha = senhaInput.value;
+  
+    validaçãoUsuario(nome, senha)
+    console.log('Nome:', nome);
+    console.log('Senha:', senha);
+  }
+  
+  function validaçãoUsuario (nome, senha) {
+    if (nome === 'Genki' && senha === 'semcasa') {
+            redirecionarParaLink('Genki')    
+
+    } else{
+        redirecionarParaLink('none')
+    }
+  
+}
+
+  function redirecionarParaLink(validacao) {
+    const GenkiUrl = './tela.html';
+    const ErroUrl = './paginas/doc/erro.html'
+    if (validacao === 'Genki'){
+        window.location.href = GenkiUrl;
+    } else {
+        window.location.href = ErroUrl;
+    }
+    
+  }
+ 
+
